f88246fc718f9bf2d51d63fd2ff61699ad25b468
monikaadsul121093 <monika.adsul12@gmail.com>
2024-01-18 00:18:52 +0530

    delete page is created

---
+++ src/main/java/PageLayer/DeletePage.java
@@ -0,0 +1,13 @@
+package PageLayer;
+
+import BaseLayer.BaseClass;
+
+public class DeletePage extends BaseClass{
+	
+	
+	public void deletePageFunctionslity()
+	{
+		System.out.println("deletePageFunctionslity");
+	}
+
+}
---
+++ src/test/java/TestLayer/DeletePageTest.java
@@ -0,0 +1,16 @@
+package TestLayer;
+
+import BaseLayer.BaseClass;
+import PageLayer.DeletePage;
+
+public class DeletePageTest extends BaseClass{
+	
+	
+	
+	public void  setUp()
+	{
+		DeletePage deletePage=new DeletePage();
+		deletePage.deletePageFunctionslity();
+	}
+
+}
